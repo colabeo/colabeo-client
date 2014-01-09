5ca6761f94d92aa3b361a35dbaa6524d9a02dd67
yellowbang <astrotabon@gmail.com>
2014-01-08 17:09:37 -0800

    Add list "#" in the All Contacts views

--- js/app/views/contact/ContactsSectionView.js
+++ js/app/views/contact/ContactsSectionView.js
@@ -19,7 +19,7 @@ define(function(require, exports, module) {
         this.abcSurfaceWidth = 30;
         this.abcSurfaceHeight = 450;
         this.curIndex = 0;
-
+        this.a2zindexArray = {};
 
         this.headerFooterLayout = new HeaderFooterLayout({
             headerSize: this.searchBarSize,
@@ -40,7 +40,7 @@ define(function(require, exports, module) {
         this.abcSurface = new Surface({
             size: [this.abcSurfaceWidth, this.abcSurfaceHeight],
             classes: ['abcButton'],
-            content: '<button id="A">A</button><button id="B">B</button><button id="C">C</button><button id="D">D</button><button id="E">E</button><button id="F">F</button><button id="G">G</button><button id="H">H</button><button id="I">I</button><button id="J">J</button><button id="K">K</button><button id="L">L</button><button id="M">M</button><button id="N">N</button><button id="O">O</button><button id="button">P</button><button id="Q">Q</button><button id="R">R</button><button id="S">S</button><button id="T">T</button><button id="U">U</button><button id="V">V</button><button id="W">W</button><button id="X">X</button><button id="Y">Y</button><button id="Z">Z</button><button id="0">#</button>',
+            content: '<button id="A">A</button><button id="B">B</button><button id="C">C</button><button id="D">D</button><button id="E">E</button><button id="F">F</button><button id="G">G</button><button id="H">H</button><button id="I">I</button><button id="J">J</button><button id="K">K</button><button id="L">L</button><button id="M">M</button><button id="N">N</button><button id="O">O</button><button id="button">P</button><button id="Q">Q</button><button id="R">R</button><button id="S">S</button><button id="T">T</button><button id="U">U</button><button id="V">V</button><button id="W">W</button><button id="X">X</button><button id="Y">Y</button><button id="Z">Z</button><button id="#">#</button>',
             properties:{
                 backgroundColor: 'rgba(160,160,160,0.0)',
                 zIndex:2
@@ -88,12 +88,28 @@ define(function(require, exports, module) {
             }
         }.bind(this));
 
+        $('body').on('click', '.abcButton button', function(e){
+            if (this.a2zindexArray[e.target.id] != undefined){
+                console.log(this.a2zindexArray[e.target.id]);
+                colabeo.contactsSection.scrollview.node.index= this.a2zindexArray[e.target.id];
+            }
+        }.bind(this));
+
+//        this.abcSurface.on('click', function(e) {
+//
+//            var target = $(e.target);
+//            if (this.a2zindexArray[target.id] != undefined){
+//                console.log(this.a2zindexArray[target.id]);
+//                colabeo.contactsSection.scrollview.node.index= this.a2zindexArray[target.id];
+//            }
+//        }.bind(this));
     }
 
     ContactsSection.prototype = Object.create(View.prototype);
     ContactsSection.prototype.constructor = ContactsSection;
 
     ContactsSection.prototype.loadContacts = function() {
+
         var firstChar;
 //        if (this.scrollview.node) this.curIndex = this.scrollview.node.index;
         this.scrollview.sequenceFrom(this.collection.map(function(item) {
@@ -102,11 +118,15 @@ define(function(require, exports, module) {
                 if (firstChar != "#"){
                     firstChar = "#";
                     isFirst = "#";
+                    var index = item.collection.indexOf(item);
+                    this.a2zindexArray[firstChar] = index;
                 }
             }
             else if (item.get('lastname') && firstChar != item.get('lastname')[0].toUpperCase()) {
                 firstChar = item.get('lastname')[0].toUpperCase();
                 isFirst = firstChar;
+                var index = item.collection.indexOf(item);
+                this.a2zindexArray[firstChar] = index;
             }
             var surface = new ContactItemView({model: item}, isFirst);
             surface.pipe(this.eventOutput);
@@ -123,11 +143,6 @@ define(function(require, exports, module) {
             }.bind(this));
         }
     };
-
-    $('body').on('click', '.abcButton button', function(e){
-        console.log(e.target.id.charCodeAt()-64);
-    }.bind(this));
-
 
     module.exports = ContactsSection;
 });

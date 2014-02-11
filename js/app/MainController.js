5d85caf4e41b5f2962875bb953b3c83cdd7892c7
jeffcolabeo <jeff@colabeo.com>
2014-02-11 00:09:17 -0800

    added oauth pop up

--- js/app/MainController.js
+++ js/app/MainController.js
@@ -378,21 +378,28 @@ define(function(require, exports, module) {
             var url;
             if (source == 'facebook') {
                 url = "/connect/facebook/email";
-//                window.location = url;
+                window.location = url;
             }
             else if (source == 'google') {
                 url = "/connect/google/profile%20email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.login%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.me";
 //                window.location = url;
             }
-            if (url)
-                $.oauthpopup({
-                    path:url,
-                    callback: function(e) {
-                        setTimeout(function() {
-                            $('div.import-contact#'+source + ':not(.done)').click().addClass('done');
-                        }, 300);
-                    }
-                });
+
+            if (url) {
+                if (Utils.isMobile()) {
+                    window.location = url;
+                } else {
+                    $.oauthpopup({
+                        path:url,
+                        callback: function(e) {
+                            setTimeout(function() {
+                                $('div.import-contact#'+source + ':not(.done)').click().addClass('done');
+                            }, 300);
+                        }
+                    });
+                }
+            }
+
         })
         $('body').on('change', '#audio, #video', function(e){
             this.appSettings.set($(e.target)[0].id, $(e.target).prop('checked'));

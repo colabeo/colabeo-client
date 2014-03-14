2e446a3e2f390b230c8a7f9308c6eccab4ff2c47
yellowbang <astrotabon@gmail.com>
2014-03-14 11:15:13 -0700

    new social-scroll-view

--- app/views/social-item-view.js
+++ app/views/social-item-view.js
@@ -1,6 +1,12 @@
+var GenericSync = require('famous/input/generic-sync');
+var MouseSync = require('famous/input/mouse-sync');
+var TouchSync = require('famous/input/touch-sync');
+var Utility = require('famous/utilities/utility');
+
 var Templates    = require('templates');
 var RowView    = require('row-view');
 var ItemView = RowView.ItemView;
+var Helpers   = require('helpers');
 
 function SocialItemView(options) {
 
@@ -22,7 +28,51 @@ SocialItemView.prototype.constructor = SocialItemView;
 // Overwrite the events functions since there is only one button event in Social-view,
 
 SocialItemView.prototype.setupEvent = function(){
+    var sync = new GenericSync(function(){
+        return this.pos;
+    }.bind(this), {
+            syncClasses:[MouseSync,TouchSync]
+        }
+    );
+    this.itemSurface.pipe(sync);
     this.itemSurface.pipe(this._eventOutput);
+    this.pos = [0,0];
+
+    sync.on('start', function() {
+        this.pos = [0,0];
+        this._directionChosen = false;
+        this.clickTimeout = setTimeout(function(){
+            this.itemSurface.setProperties({backgroundColor: 'rgba(255,255,255,0.1)'});
+        }.bind(this),100);
+    }.bind(this));
+
+    sync.on('update', function(data) {
+        if (this.clickTimeout) {
+            clearTimeout(this.clickTimeout);
+            delete this.clickTimeout;
+        }
+        this.itemSurface.setProperties({backgroundColor: 'transparent'});
+        this.pos = data.p;  // the displacement from the start touch point.
+        if( Helpers.isMobile() && !this._directionChosen ) {
+            var diffX = Math.abs( this.pos[0] ),
+                diffY = Math.abs( this.pos[1] );
+            this.direction = diffX > diffY ? Utility.Direction.X : Utility.Direction.Y;
+            this._directionChosen = true;
+            if (this.direction == Utility.Direction.X) {
+                this.itemSurface.unpipe(this._eventOutput);
+            }
+            else {
+                this.itemSurface.pipe(this._eventOutput);
+            }
+        }
+    }.bind(this));
+
+    sync.on('end', function() {
+        setTimeout(function(){
+            this.itemSurface.setProperties({backgroundColor: 'transparent'});
+        }.bind(this),300);
+    }.bind(this));
+    if ( Helpers.isMobile() && this.direction != Utility.Direction.X) return;
 };
 
 SocialItemView.prototype.buttonsClickEvents = function() {
